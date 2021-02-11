3f18dbc20640d23c044b328bba73f823eca6b912
ENG19CT0012-HEMA <75926191+ENG19CT0012-HEMA@users.noreply.github.com>
2021-02-11 14:20:15 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,13 @@
 //Write a program to find the sum of n different numbers using 4 functions
+ Include <stdio.h>
+Int main()
+{
+Int num1, num2,sum;
+printf(“Enter first number : “);
+scanf(“%d”, &num1);
+printf(“enter second number:”);
+scanf(“%d”,&num2);
+ 
+sum=num1+num2;
+printf(“sum of the entered numbers : %d”,sum);
+return0;
