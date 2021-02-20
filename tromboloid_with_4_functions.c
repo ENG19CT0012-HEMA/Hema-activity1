a421173f71022907a4cd753994ea0e47a9fef846
ENG19CT0012-HEMA <75926191+ENG19CT0012-HEMA@users.noreply.github.com>
2021-02-20 14:21:55 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,25 @@
 //WAP to find the volume of a tromboloid using 4 functions.
+#include<stdio.h>
+float input(char b)
+printf(“Enter the volume of %c:”,b)
+{
+Float volume(float h,float d,float b)
+{
+Float vol;
+vol=(1.0/3*((h*d)+d))/b;
+Return vol;
+}
+Void output(float res)
+{
+printf(“Volume of a tromboloid: %f” ,res);
+}
+int main() 
+{
+float h,d,b,res;
+h=input(‘h’);
+d=input(‘d’);
+b=input(‘b’);
+res=volume(h,d,b);
+output(res);
+return 0;
+}
